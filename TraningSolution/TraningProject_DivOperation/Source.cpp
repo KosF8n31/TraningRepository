fecf69d7563cb318c5cd984849003432172ab860
KosF8n31 <iLYAkOKOREW@yandex.ru>
2023-03-01 23:34:38 +0300

    Исправил деление на 0

--- TraningSolution/TraningProject_DivOperation/Source.cpp
+++ TraningSolution/TraningProject_DivOperation/Source.cpp
@@ -2,9 +2,12 @@
 int main() {
 	int a, b;
 	float div;
-	scanf_s("%d %d", &a, &b);
-	div = a / b;
-	printf_s("%f\n", div);
+	scanf("%d %d", &a, &b);
+	if (b == 0) {
+		printf("На ноль делят только в высшей математике!");
+	}
+	else div = a / b;
+	printf("%f\n", div);
 
 	return 0;
 }
