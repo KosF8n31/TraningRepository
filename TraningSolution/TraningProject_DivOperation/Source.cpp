3b2e5748ceb47fd9660153671ab289b9e5aa3e8b
KosF8n31 <iLYAkOKOREW@yandex.ru>
2023-03-02 00:04:32 +0300

    Исправил ошибку с отрицательным знаменателем

--- TraningSolution/TraningProject_DivOperation/Source.cpp
+++ TraningSolution/TraningProject_DivOperation/Source.cpp
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <locale.h>
+
 int main() {
+	setlocale(LC_ALL, "rus");
 	int a, b;
 	float div;
-	scanf("%d %d", &a, &b);
+	scanf_s("%d %d", &a, &b);
 	if (b == 0) {
 		printf("На ноль делят только в высшей математике!");
 	}
-	else div = a / b;
-	printf("%f\n", div);
-
+	else {
+		div = a / b;
+		printf_s("%f\n", div);
+	}
 	return 0;
 }
